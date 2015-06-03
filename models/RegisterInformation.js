9f1a3dccc3c2cbb9186a587bcdeab19ee998ee19
Fredrik Lundberg <fredrik.lundberg@registercentrum.se>
2015-06-03 17:21:14 +0200

    Changed the location type field
    Since the `Location` field type had the wrong format multiple fields has
    been added instead to adhere more to the swedish address format.
    Longitude and latitude are therefore stored in separate fields which has
    to be input manually for now.
    
    Minor changes to google maps integration has also been done e.g.
    disabled scrolling.

--- models/RegisterInformation.js
+++ models/RegisterInformation.js
@@ -1,5 +1,6 @@
 var keystone = require('keystone'),
-	Types = keystone.Field.Types;
+	Types = keystone.Field.Types,
+	longLatRegEx = /^($|(\-?\d+(\.\d+)?)$)/;
 
 /**
  * Start Page Model
@@ -15,11 +16,30 @@ RegisterInformation.add({
 	name: { type: String, required: true },
 	description: { type: Types.Textarea, height: 150 },
 	email: { type: Types.Email },
-	location: { type: Types.Location },
-	phone: { type: String },
+	// location: { type: Types.Location },
+	phone: { type: String }
+	}, 'Location', {
+	location: {
+		street1: { type: String, label: 'Street 1' },
+		street2: { type: String, label: 'Street 2', collapse: true },
+		zipCode: { type: String, label: 'Zip Code' },
+		city: { type: String, label: 'City' }
+	},
+	showMap: { type: Boolean, note: 'Shows a map on the contact page' },
+	latitude: { type: String, /*required: true,*/ dependsOn: { showMap: true } },
+	longitude: { type: String, /*required: true,*/ dependsOn: { showMap: true } },
 	isPortal: { type: Boolean, note: 'Check here if the register is a portal of several registers' },
-	subRegisters: { type: Types.Relationship, ref: 'StratumRegister', many: true, dependsOn: {isPortal: true} },
-	showMap: { type: Boolean, note: 'Shows a map on the contact page' }
+	subRegisters: { type: Types.Relationship, ref: 'StratumRegister', many: true, dependsOn: {isPortal: true} }
 });
  
+RegisterInformation.schema.path('latitude').validate(function(value){
+	return longLatRegEx.test(value);
+});
+RegisterInformation.schema.path('longitude').validate(function(value){
+	return longLatRegEx.test(value);
+});
+RegisterInformation.schema.virtual('geo').get(function() {
+	return this.showMap && this.latitude && this.longitude ? [this.latitude, this.longitude] : null;
+});
+
 RegisterInformation.register();
