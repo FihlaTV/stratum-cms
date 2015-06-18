0353cbf4ae11be36dad36ca9844867e843d87275
Fredrik Lundberg <fredrik.lundberg@registercentrum.se>
2015-06-18 13:41:08 +0200

    Changed title and navbar to brand name

--- routes/middleware.js
+++ routes/middleware.js
@@ -31,6 +31,7 @@ exports.initLocals = function(req, res, next) {
 	];
 	locals.user = req.user;
 	locals.lastCommit = keystone.get('last commit');
+	locals.brand = keystone.get('brand');
 	async.series({
 		loadCategories: function(cb) {
 			keystone.list('ContentCategory').model
