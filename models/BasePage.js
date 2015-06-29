b8085edf72a6f4f865c6d346bbb32961b60c5b8c
Fredrik Lundberg <fredrik.lundberg@registercentrum.se>
2015-06-29 12:53:26 +0200

    Added sub page list and cleaned up in BasePage

--- models/BasePage.js
+++ models/BasePage.js
@@ -29,17 +29,12 @@ BasePage.add({
 	subtitle: {
 		type: String
 	},
-	// category: { type: Types.Relationship, ref: 'ContentCategory', many: false },
-	// menu: { type: Types.Relationship, ref: 'MenuBlock', many: false, label: 'Menu Block' },
-	// parentPage: { type: Types.Relationship, ref: 'BasePage', many: false, label: 'Sub Page to', filters: { menu: ':menu' }, dependsOn: {hasSubpages: false} },
 	state: {
 		type: Types.Select,
 		options: 'draft, published, archived',
 		default: 'draft',
 		index: true
 	},
-	// author: { type: Types.Relationship, ref: 'User', index: true },
-	// publishedDate: { type: Types.Date, index: true, dependsOn: { state: 'published' } },
 	image: {
 		type: Types.CloudinaryImage
 	},
@@ -51,9 +46,6 @@ BasePage.add({
 		ref: 'StratumWidget',
 		many: false
 	},
-	// content: {
-	// brief: { type: Types.Textarea, height: 150 },
-	// },
 	content: {
 		type: Types.Markdown,
 		height: 400,
@@ -66,38 +58,11 @@ BasePage.add({
 	}
 });
 
-// BasePage.schema.virtual('content.full').get(function() {
-// 	return this.content.extended || this.content.brief;
-// });
-// BasePage.schema.virtual('hasSubpages').get(function(){
-// 	return !!this.subPages;
-// });
-// BasePage.relationship({
-// 	path: 'subPages',
-// 	ref: 'BasePage',
-// 	refPath: 'parentPage'
-// });
-// BasePage.schema.path('parentPage').validate(function(value){
-// 	return !(value && value.equals(this._id));
-// }, 'Cannot be sub page to itself, please select another page as parent');
-// BasePage.schema.path('menu').validate(function(value, respond){
-// 	if(!value || !this.parentPage){
-// 		respond(true);
-// 	}
-// 	keystone.list('BasePage').model
-// 		.findOne()
-// 		.where('_id', this.parentPage)
-// 		.where('menu', value)
-// 		.exec(function(err, parent){
-// 			respond(!err && !!parent);
-// 		});
-// 	// return false;
-// }, 'Cannot be a sub page to a page which is in another menu block');
-// BasePage.defaultSort = 'category';
-// BasePage.defaultColumns = 'title, state|20%, category|20%, publishedDate|20%';
 BasePage.register();
 
+//Page
 var Page = new keystone.List('Page', {
+	// sortContext: 'MenuBlock:pages', //sortContext not working properly
 	inherits: BasePage,
 	hidden: false,
 	nocreate: false,
@@ -105,10 +70,34 @@ var Page = new keystone.List('Page', {
 });
 Page.add({
 	menu: {
-		// category: { type: Types.Relationship, ref: 'ContentCategory', many: false },
 		type: Types.Relationship,
 		ref: 'MenuBlock',
-		many: false
+		many: false,
+		required: true,
+		initial: true
 	}
 });
+Page.relationship({
+	path: 'subPages',
+	ref: 'SubPage',
+	refPath: 'page'
+});
 Page.register();
+
+//SubPage
+var SubPage = new keystone.List('SubPage', {
+	inherits: BasePage,
+	hidden: false,
+	nocreate: false,
+	nodelete: false
+});
+SubPage.add({
+	page: {
+		type: Types.Relationship,
+		ref: 'Page',
+		many: false,
+		initial: true,
+		required: true
+	}
+});
+SubPage.register();
