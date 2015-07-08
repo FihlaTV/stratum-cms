7938977fc07e0a2930f2d77aba327e5dff2b5256
Fredrik Lundberg <fredrik.lundberg@registercentrum.se>
2015-07-08 09:25:32 +0200

    Masonry fixes

--- public/js/views/news.js
+++ public/js/views/news.js
@@ -1,8 +1,13 @@
-(function(){
+(function() {
 	// var $news = $('post');
 	var $news = $('.news-list');
-	$news.imagesLoaded( function() {
-		$news.masonry({ itemSelector: '.news-list-item' });
+	$news.imagesLoaded(function() {
+		$news.masonry({
+			columnWidth: '.news-list-sizer',
+			itemSelector: '.news-list-item',
+			percentPosition: true,
+			gutter: '.news-list-gutter'
+		});
 	});
 
 	// $news.masonry();
