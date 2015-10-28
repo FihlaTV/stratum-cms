5bff25f247d3bb4586807865807484bc55abafad
Fredrik Lundberg <fredrik.lundberg@registercentrum.se>
2015-10-28 17:16:00 +0100

    Added actions

---
+++ client/scripts/actions/actions.js
@@ -0,0 +1,22 @@
+export const INPUT_PERSONAL_NUMBER = 'INPUT_PERSONAL_NUMBER';
+export const SET_LOGIN_METHOD = 'SET_LOGIN_METHOD';
+
+export const LoginMethods = {
+    BANDK_ID: 'BANK_ID',
+    SITH_CARD: 'SITHS_CARD'
+};
+
+export function setLoginMethod(loginMethod) {
+    return {
+        type: SET_LOGIN_METHOD,
+        loginMethod
+    };
+}
+
+export function inputPersonalNumber(personalNumber) {
+    return {
+        type: INPUT_PERSONAL_NUMBER,
+        personalNumber
+    };
+
+}
