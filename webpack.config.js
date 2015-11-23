dee81c6909996fb8fde9510458df5d5a885ba6c7
Fredrik Lundberg <fredrik.lundberg@registercentrum.se>
2015-11-23 16:08:17 +0100

    Fixed did not follow lint standard

--- webpack.config.js
+++ webpack.config.js
@@ -33,22 +33,22 @@ module.exports = {
 			test: /\.js$/,
 			loader: 'babel',
 			query: {
-				"stage": 2,
-				"env": {
-				  "development": {
-				    "plugins": [
-				      "react-transform"
-				    ],
-				    "extra": {
-				      "react-transform": {
-				        "transforms": [{
-				          "transform":  "react-transform-hmr",
-				          "imports": ["react"],
-				          "locals":  ["module"]
-				        }]
-				      }
-				    }
-				  }
+				'stage': 2,
+				'env': {
+					'development': {
+						'plugins': [
+							'react-transform'
+						],
+						'extra': {
+							'react-transform': {
+								'transforms': [{
+									'transform': 'react-transform-hmr',
+									'imports': ['react'],
+									'locals': ['module']
+								}]
+							}
+						}
+					}
 				}
 			},
 			exclude: /node_modules/,
