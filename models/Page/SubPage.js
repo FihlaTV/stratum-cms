203499ce01932d1b183e1eb6e17fe77a80c62a60
Fredrik Lundberg <fredrik.lundberg@registercentrum.se>
2015-12-18 09:54:01 +0100

    Added pre save middleware which checks for page relations on sub page

--- models/Page/SubPage.js
+++ models/Page/SubPage.js
@@ -1,6 +1,7 @@
 var keystone = require('keystone'),
 	Types = keystone.Field.Types,
-	BasePage = keystone.list('BasePage');
+	BasePage = keystone.list('BasePage'),
+	async = require('async');
 
 /**
  * Sub Page Model
@@ -25,4 +26,44 @@ SubPage.add({
 		required: true
 	}
 });
+SubPage.schema.pre('save', function (next) {
+	var postPage = this, Page = keystone.list('Page'), context = {};
+	async.series({
+		checkSubPage: function (_next) {
+			SubPage.model
+				.findOne()
+				.where('_id', postPage._id)
+				.populate('page')
+				.exec(function (err, prePage) {
+						if (prePage && prePage.page._id.equals(postPage.page)) {
+							console.log('same, do nothing');
+							//Same do nothing
+						} else {
+							context.decreasePrePage = !!prePage;
+							context.incrementPostPage = true;
+						}
+					_next(err);
+				});
+		},
+		increasePostPage: function(_next){
+			if(!context.incrementPostPage){
+				_next();
+				return;
+			}
+			console.log('increase post page');
+			_next();
+		},
+		decreasePrePage: function(_next){
+			if(!context.decreasePrePage){
+				_next();
+				return;
+			}
+			console.log('decrease pre page');
+			_next();
+		}
+	}, function (err) {
+		next();
+	});
+});
+
 SubPage.register();
