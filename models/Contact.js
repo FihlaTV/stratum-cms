959da3076050554e2928f0b9ec16bbfab7c7ec3d
Fredrik Lundberg <fredrik.lundberg@registercentrum.se>
2016-01-12 14:26:05 +0100

    Added description virtual
    Selects note instead of title if this is available

--- models/Contact.js
+++ models/Contact.js
@@ -10,16 +10,22 @@ var Contact = new keystone.List('Contact');
 
 Contact.add({
 	name: { type: Types.Name, required: true, index: true },
-	email: { type: Types.Email, initial: true, required: true, index: true },
+	email: { type: Types.Email, initial: true },
 	image: { type: Types.CloudinaryImage },
 	phone: { type: String },
 	title: { type: String, label: 'Work Title'},
+	note: { type: Types.Textarea, collapse: true },
 	showOnContactPage: { type: Boolean }
 });
 
 /**
  * Registration
  */
+
+
+Contact.schema.virtual('description').get(function() {
+	return this.get('note') || this.get('title');
+});
 
 Contact.defaultColumns = 'name, email, showOnContactPage';
 Contact.register();
