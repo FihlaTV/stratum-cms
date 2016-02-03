73ff06cc43fc1f04f526de6302d2e0e03ce21371
Fredrik Lundberg <fredrik.lundberg@registercentrum.se>
2016-02-03 17:55:42 +0100

    Changed status messages

--- client/scripts/components/BIDStatusMessage.jsx
+++ client/scripts/components/BIDStatusMessage.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 
 const StatusMessages = {
     NO_CLIENT: 'NO_CLIENT',
-    OUTSTANDING_CLIENT: 'OUTSTANDING_CLIENT'
+    OUTSTANDING_TRANSACTION: 'OUTSTANDING_TRANSACTION',
+	USER_SIGN: 'USER_SIGN',
+	COMPLETE: 'COMPLETE'
 };
 
 const BIDStatusMessage = ({
@@ -10,11 +12,14 @@ const BIDStatusMessage = ({
 }) => {
     switch (status) {
         case StatusMessages.NO_CLIENT:
-            return <span>Hej svej no client</span>
-        case StatusMessages.OUTSTANDING_CLIENT:
-            return <span>Var god do something plsz...</span>
+        case StatusMessages.OUTSTANDING_TRANSACTION:
+            return <span>Var god starta "Mobilt BankID"-appen i din smart phone</span>
+		case StatusMessages.USER_SIGN:
+			return <span>Skriv in din personliga kod i din smart phone</span>
+		case StatusMessages.COMPLETE:
+			return <span>Identifierad mot BankID, loggar in Stratum</span>
         default:
-            return <span>Saknar status...</span>
+            return <span>{status}</span>
     }
 }
 
