b80165770a213abd10d57a2f74ad779c6b3f316f
Fredrik Lundberg <fredrik.lundberg@registercentrum.se>
2016-02-08 16:13:20 +0100

    Added advanced settings heading and changed static link to depend on static and section

--- models/MenuBlock.js
+++ models/MenuBlock.js
@@ -20,40 +20,44 @@ MenuBlock.add({
 		type: String,
 		required: true
 	},
-	registerSpecific: {
-		type: Boolean,
-		note: 'Only visible to logged in users'
-	},
-	static: {
-		type: Boolean,
-		note: 'Advanced option: Check this if this menu item should link to a specific URL'
-	},
-	section: {
-		type: Types.Select,
-		options: [
-			{ value: 'external', label: 'External Link' },
-			{ value: 'news', label: 'News' },
-			{ value: 'questions', label: 'Questions' },
-			{ value: 'contact', label: 'Contact Page' }
-		],
-		dependsOn: {
-			static: true
+},
+	{ heading: 'Advanced settings' },
+	{
+		registerSpecific: {
+			type: Boolean,
+			note: 'Only visible to logged in users'
+		},
+		static: {
+			type: Boolean,
+			note: 'Advanced option: Check this if this menu item should link to a specific URL'
+		},
+		section: {
+			type: Types.Select,
+			options: [
+				{ value: 'external', label: 'External Link' },
+				{ value: 'news', label: 'News' },
+				{ value: 'questions', label: 'Questions' },
+				{ value: 'contact', label: 'Contact Page' }
+			],
+			dependsOn: {
+				static: true
+			}
+		},
+		staticLink: {
+			type: Types.Url,
+			dependsOn: {
+				static: true,
+				section: 'external'
+			}
 		}
-	},
-	staticLink: {
-		type: Types.Url,
-		dependsOn: {
-			section: 'external'
-		}
-	}
-});
+	});
 MenuBlock.relationship({
 	path: 'pages',
 	ref: 'Page',
 	refPath: 'menu'
 });
 
-MenuBlock.schema.virtual('key').get(function() {
+MenuBlock.schema.virtual('key').get(function () {
 	return this.get('section') || this.get('slug');
 });
 
