1546706a2cf4ed23b2a538f2a313ba713b4d2b3f
Fredrik Lundberg <fredrik.lundberg@registercentrum.se>
2016-02-09 16:36:49 +0100

    FIxed formatting

--- client/scripts/components/BIDStatusMessage.jsx
+++ client/scripts/components/BIDStatusMessage.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
 const StatusMessages = {
-    NO_CLIENT: 'NO_CLIENT',
-    OUTSTANDING_TRANSACTION: 'OUTSTANDING_TRANSACTION',
+	NO_CLIENT: 'NO_CLIENT',
+	OUTSTANDING_TRANSACTION: 'OUTSTANDING_TRANSACTION',
 	USER_SIGN: 'USER_SIGN',
 	COMPLETE: 'COMPLETE'
 };
@@ -10,17 +10,17 @@ const StatusMessages = {
 const BIDStatusMessage = ({
     status
 }) => {
-    switch (status) {
-        case StatusMessages.NO_CLIENT:
-        case StatusMessages.OUTSTANDING_TRANSACTION:
-            return <span>Var god starta "Mobilt BankID"-appen i din smart phone</span>
+	switch (status) {
+		case StatusMessages.NO_CLIENT:
+		case StatusMessages.OUTSTANDING_TRANSACTION:
+			return <span>Var god starta "Mobilt BankID"-appen i din smart phone</span>
 		case StatusMessages.USER_SIGN:
 			return <span>Skriv in din personliga kod i din smart phone</span>
 		case StatusMessages.COMPLETE:
 			return <span>Identifierad mot BankID, loggar in Stratum</span>
-        default:
-            return <span>{status}</span>
-    }
+		default:
+			return <span>{status}</span>
+	}
 }
 
 export default BIDStatusMessage;
