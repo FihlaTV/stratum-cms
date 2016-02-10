e9e75434be36844da7fd324256620373b209bccb
Fredrik Lundberg <fredrik.lundberg@registercentrum.se>
2016-02-10 10:56:11 +0100

    Added support for Redux Devtools Chrome extension

--- client/scripts/index.jsx
+++ client/scripts/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import { Provider } from 'react-redux';
 import Application from './containers/App.jsx';
@@ -8,7 +8,10 @@ import loginApp from './reducers/reducers.js';
 
 const mainContainer = document.getElementById('login-page');
 
-let store = applyMiddleware(thunkMiddleware)(createStore)(loginApp);
+let store = compose(
+		applyMiddleware(thunkMiddleware),
+	    window.devToolsExtension ? window.devToolsExtension() : f => f
+	)(createStore)(loginApp);
 
 if(mainContainer){
 	render(
