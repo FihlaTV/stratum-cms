d5d7a6307cb83a1b5affcdd9539a0512e38b9c12
Fredrik Lundberg <fredrik@ablu.se>
2016-02-12 16:12:51 +0100

    Updated webpack production config to work with newest version

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -5,31 +5,37 @@ var node_modules_dir = path.resolve(__dirname, 'node_modules');
 require('dotenv').load();
 
 module.exports = {
-	entry: {
-		app: path.resolve(__dirname, 'client/scripts/index.jsx'),
-		vendors: ['react', 'jquery', 'moment', 'react-bootstrap']
-	},
-	output: {
-		path: path.resolve(__dirname, 'public/js/dist'),
-		filename: 'bundle.js'
-	},
-	plugins: [
-		new webpack.DefinePlugin({
-			'process.env': Object.keys(process.env).reduce(function(o, k) {
-				//Whitelist to variables only prefixed with CLIENT_
-				if (k.indexOf('CLIENT_') === 0) {
-					o[k] = JSON.stringify(process.env[k]);
-				}
-				return o;
-			}, {})
-		}),
-		new webpack.optimize.CommonsChunkPlugin('vendors', 'vendors.js')
-	],
-	module: {
-		loaders: [{
-			test: /\.jsx{0,1}$/,
-			exclude: [node_modules_dir],
-			loader: 'babel'
-		}]
-	}
+    entry: {
+        app: path.resolve(__dirname, 'client/scripts/index.jsx'),
+        vendors: ['react', 'jquery', 'moment', 'react-bootstrap']
+    },
+    output: {
+        path: path.resolve(__dirname, 'public/js/dist'),
+        filename: 'bundle.js'
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env': Object.keys(process.env).reduce(function (o, k) {
+                //Whitelist to variables only prefixed with CLIENT_
+                if (k.indexOf('CLIENT_') === 0) {
+                    o[k] = JSON.stringify(process.env[k]);
+                }
+                return o;
+            }, {})
+        }),
+        new webpack.optimize.CommonsChunkPlugin('vendors', 'vendors.js')
+    ],
+    module: {
+        loaders: [{
+            test: /\.jsx{0,1}$/,
+            loader: 'babel-loader',
+            exclude: [node_modules_dir],
+            query: {
+                presets: ['es2015', 'react']
+            }
+        }]
+    },
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    }
 };
