3eb0f35e0c2b8a6c7133d9761ec9ec3efd2cc8cb
Fredrik Lundberg <fredrik.lundberg@registercentrum.se>
2016-02-18 10:15:27 +0100

    Refined font loaders

--- webpack.config.js
+++ webpack.config.js
@@ -56,12 +56,18 @@ module.exports = {
                 test: /\.less$/,
                 loader: "style!css?-url!less"
             }, {
-                test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-                loader: "url?limit=10000&mimetype=application/font-woff"
-            }, {
-                test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-                loader: "file"
-            }
+				test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
+				loader: 'url?limit=10000&mimetype=application/font-woff'
+			}, {
+				test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+				loader: 'url?limit=10000&mimetype=application/octet-stream'
+			}, {
+				test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+				loader: 'file'
+			}, {
+				test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+				loader: 'url?limit=10000&mimetype=image/svg+xml'
+			}
         ]
     },
     resolve: {
