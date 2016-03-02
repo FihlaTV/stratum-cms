d9bd6a91cc459e2b788625f2de192265ddd9ce09
Fredrik Lundberg <fredrik@ablu.se>
2016-03-02 18:29:31 +0100

    Added masonry as a npm package
    Currently namespaced in window since the view script is loaded
    separately.
    
    Minor visual adjustments to adapt the news list to the new style.

--- public/js/views/news.js
+++ public/js/views/news.js
@@ -1,14 +1,10 @@
-(function() {
-	// var $news = $('post');
-	var $news = $('.news-list');
-	$news.imagesLoaded(function() {
-		$news.masonry({
-			columnWidth: '.news-list-sizer',
-			itemSelector: '.news-list-item',
-			percentPosition: true,
-			gutter: '.news-list-gutter'
-		});
-	});
-
-	// $news.masonry();
-}());
+(function () {
+    imagesLoaded('.news-list', function () {
+        new Masonry('.news-list', {
+            columnWidth: '.news-list-sizer',
+            itemSelector: '.news-list-item',
+            percentPosition: true,
+            gutter: '.news-list-gutter'
+        });
+    });
+} ());
