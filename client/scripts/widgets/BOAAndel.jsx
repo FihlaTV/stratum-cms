4c28ff4e6b09ca4e45cdf81e11301b927d7bef38
Fredrik Lundberg <fredrik.lundberg@registercentrum.se>
2016-03-16 16:35:17 +0100

    Added actual values to sample widget

--- client/scripts/widgets/BOAAndel.jsx
+++ client/scripts/widgets/BOAAndel.jsx
@@ -5,9 +5,9 @@ const BOAAndel = ({
 }) => {
 	return (
 		<StratumSimpleData
-			url="//stratum.registercentrum.se/api/aggregate/BOA/UnitPraxis/Total/count(UP_UnitName)?APIKey=bK3H9bwaG4o%3D"
+			url="//stratum.registercentrum.se/api/aggregate/BOA/FirstVisitPatient/Total/count?APIKey=bK3H9bwaG4o%3D"
 			>
-			<p>En rolig andel istsället</p>
+			<p>antal registrerade i registret</p>
 		</StratumSimpleData>
 	);
 }
