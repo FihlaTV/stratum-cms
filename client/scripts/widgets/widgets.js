b9598095b5681edb020260c56a6d6a108c1edd10
Fredrik Lundberg <fredrik.lundberg@registercentrum.se>
2016-03-16 14:12:03 +0100

    Added index file for widgets and adapted loading api
    `widgets.js` contains metadata information of all react keystone widgets
    and is supposed to be seen as a specification for which widgets that can
    be initiated by the admin ui. Therefore the keystone widget loader is
    now only looking in the index file instead of searching in files.

---
+++ client/scripts/widgets/widgets.js
@@ -0,0 +1,10 @@
+/**	
+ * Used as a index ES5 for all Widgets
+ * 
+ * Supposed to be called from the backend in order to list all available Widgets
+ * and expose them to the admin ui for example
+ */
+
+module.exports = {
+	'BOAArtrosSkola': { id: 'BOAArtrosSkola', description: '' }
+};
--- routes/api/load-widgets.js
+++ routes/api/load-widgets.js
@@ -1,28 +1,15 @@
 var widgets = require('../../utils/keystone-widgets');
+var widgetIndex = require('../../client/scripts/widgets/widgets');
 
 exports = module.exports = function(req, res) {
-	// stratum.loadRegisters(function(err, context) {
-	// 	if (err) {
-	// 		return res.apiResponse({
-	// 			sucess: false,
-	// 			err: err
-	// 		});
-	// 	} else {
-	// 		return res.apiResponse({
-	// 			success: true,
-	// 			data: {
-	// 				removed: context.nRemoved,
-	// 				new: context.nNew
-	// 			}
-	// 		});
-	// 	}
-	// });
-	widgets.loadWidgets(function(err, files) {
-		return res.apiResponse({
-			success: !!err,
-			data: 
+	widgets.loadWidgets({
+		widgets: Object.keys(widgetIndex),
+		callback: function(err, files) {
+			return res.apiResponse({
+				success: !err,
+				data:
 				err || files
-			
-		});
+			});
+		}
 	});
 };
