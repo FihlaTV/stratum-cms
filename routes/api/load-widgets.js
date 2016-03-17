008a46be4eece3cffc0f3125edb9aaf37dc37ae3
Fredrik Lundberg <fredrik.lundberg@registercentrum.se>
2016-03-17 16:01:12 +0100

    Keystone Widget loading call now supports meta data descriptions

--- routes/api/load-widgets.js
+++ routes/api/load-widgets.js
@@ -3,7 +3,7 @@ var widgetIndex = require('../../client/scripts/widgets/widgets');
 
 exports = module.exports = function(req, res) {
 	widgets.loadWidgets({
-		widgets: Object.keys(widgetIndex),
+		widgets: widgetIndex,
 		callback: function(err, files) {
 			return res.apiResponse({
 				success: !err,
--- utils/keystone-widgets.js
+++ utils/keystone-widgets.js
@@ -10,75 +10,91 @@ exports.loadWidgets = function(opts) {
 		callback = _.isFunction(opts) ? opts : opts.callback,
 		context = {
 			widgetsInDB: [],
-			widgets: opts.widgets
+			updatedWidgets: [],
+			widgets: _.keys(opts.widgets),
+			widgetsFull: opts.widgets
 		};
 
 	async.series({
-			readFiles: function(next) {
-				if(context.widgets){
-					next();
-					return;
-				}
-				fs.readdir(path, function(err, files) {
-					if (!err) {
-						context.widgets = files.reduce(function(prev, file) {
-							if (file.substr(-3) === '.js') {
-								prev.push(file.slice(0, -3));
-							}
-							return prev;
-						}, []);
-					}
-					next(err);
-				});
-			},
-			findNewWidgets: function(next) {
-				KeystoneWidget.model.find()
-					.where('name').in(context.widgets)
-					.select('name')
-					.exec(function(err, widgets) {
-						if (!err) {
-							context.newWidgets = _.difference(context.widgets, _.pluck(widgets, 'name'));
+		readFiles: function(next) {
+			if (context.widgets) {
+				next();
+				return;
+			}
+			fs.readdir(path, function(err, files) {
+				if (!err) {
+					context.widgets = files.reduce(function(prev, file) {
+						if (file.substr(-3) === '.js') {
+							prev.push(file.slice(0, -3));
 						}
-						next(err);
+						return prev;
+					}, []);
+				}
+				next(err);
+			});
+		},
+		findNewWidgets: function(next) {
+			async.each(context.widgetsFull, function(widget, cb) {
+				KeystoneWidget.model.findOne()
+					.where('name', widget.id)
+					.select('name description')
+					.exec(function(err, dWidget) {
+						if (err) {
+							cb(err);
+							return;
+						}
+						if (dWidget && dWidget.description !== widget.description) {
+							//Update description
+							dWidget.description = widget.description;
+							context.updatedWidgets.push(dWidget.name);
+							dWidget.save(cb);
+						} else if (!dWidget) { // New widget
+							var newWidget = new KeystoneWidget.model({
+								name: widget.id,
+								description: widget.description,
+								removed: false
+							});
+							context.newWidgets.push(widget.id);
+							newWidget.save(cb);
+						} else {
+							cb();
+						}
 					});
-			},
-			addRemovedWidgets: function(next) {
-				KeystoneWidget.model.update({
-					name: {
-						$in: context.widgets
-					},
-					removed: true
-				}, {
+			}, next);
+		},
+		addRemovedWidgets: function(next) {
+			KeystoneWidget.model.update({
+				name: {
+					$in: context.widgets
+				},
+				removed: true
+			}, {
 					removed: false
 				}, {
 					multi: true
 				}, next);
-			},
-			addNewWidgets: function(next) {
-				async.each(context.newWidgets, function(widget, cb) {
-					var newWidget = new KeystoneWidget.model({
-						name: widget,
-						removed: false
-					});
-					newWidget.save(cb);
-				}, next);
-			},
-			removeOldWidgets: function(next) {
-				KeystoneWidget.model.update({
-					name: {
-						$nin: context.widgets
-					},
-					removed: {
-						$ne: true
-					}
-				}, {
+		},
+		removeOldWidgets: function(next) {
+			KeystoneWidget.model.update({
+				name: {
+					$nin: context.widgets
+				},
+				removed: {
+					$ne: true
+				}
+			}, {
 					removed: true
 				}, {
 					multi: true
 				}, next);
-			}
-		},
+		}
+	},
 		function(err) {
-			callback(err, context);
+			callback(err, {
+				indexed: context.widgets,
+				// widgetsInDB: context.widgetsInDB,
+				updatedWidgets: context.updatedWidgets,
+				newWidgets: context.newWidgets
+			});
 		});
 };
