3338e9651ed4ecf4b2417906c7a4383adb8054ee
Fredrik Lundberg <fredrik.lundberg@registercentrum.se>
2016-03-21 16:05:20 +0100

    Sample widgets of one simple and one complex variant of stratumsimpledata

--- client/scripts/widgets/BOAAndel.jsx
+++ client/scripts/widgets/BOAAndel.jsx
@@ -1,15 +1,31 @@
 import React, { Component, PropTypes } from 'react';
 import StratumSimpleData from './StratumSimpleData';
 
-const BOAAndel = ({
-}) => {
-	return (
-		<StratumSimpleData
-			url="//stratum.registercentrum.se/api/aggregate/BOA/FirstVisitPatient/Total/count?APIKey=bK3H9bwaG4o%3D"
-		>
-			<p>antal registrerade i registret</p>
-		</StratumSimpleData>
-	);
+
+class BOAAndel extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			loadComplete: false
+		};
+	}
+	onComplete(error) {
+		this.setState({
+			error: error,
+			loadComplete: true
+		});
+	}
+	render() {
+		return (
+			<div>
+				<StratumSimpleData
+					url="//stratum.registercentrum.se/api/aggregate/BOA/FirstVisitPatient/Total/count?APIKey=bK3H9bwaG4o%3D"
+					onLoadComplete={this.onComplete.bind(this) }
+					/>
+				<p>{this.state.loadComplete && this.props.description}</p>
+			</div>
+		);
+	}
 }
 
 export default BOAAndel
--- client/scripts/widgets/BOAArtrosSkola.jsx
+++ client/scripts/widgets/BOAArtrosSkola.jsx
@@ -1,20 +1,16 @@
 import React, { Component, PropTypes } from 'react';
 import StratumSimpleData from './StratumSimpleData';
 
-class BOAArtrosSkola extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {};
-	}
-	render() {
-		return (
-			<StratumSimpleData
-				url="//stratum.registercentrum.se/api/aggregate/BOA/UnitPraxis/Total/count(UP_UnitName)?APIKey=bK3H9bwaG4o%3D"
+const BOAArtrosSkola = ({
+	description
+}) => {
+	return (
+		<StratumSimpleData
+			url="//stratum.registercentrum.se/api/aggregate/BOA/UnitPraxis/Total/count(UP_UnitName)?APIKey=bK3H9bwaG4o%3D"
 			>
-				<p>{this.props.description}</p>
-			</StratumSimpleData>
-		);
-	}
+			<p>{description}</p>
+		</StratumSimpleData>
+	);
 }
 
 BOAArtrosSkola.defaultProps = {};
