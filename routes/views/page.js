2dc480107a18e6ff476483337de910a2616ddb1e
Fredrik Lundberg <fredrik.lundberg@registercentrum.se>
2016-03-24 11:18:53 +0100

    Clean up obsolete code

--- routes/views/page.js
+++ routes/views/page.js
@@ -7,7 +7,6 @@ exports = module.exports = function(req, res) {
 
 	locals.stratumServer = keystone.get('stratum server');
 	locals.breadcrumbs = [];
-	// locals.widget = 'reportlist';
 	locals.widget = {};
 	locals.section = req.params.page;
 	locals.data = {
@@ -27,7 +26,6 @@ exports = module.exports = function(req, res) {
 		keystone.list('MenuBlock').model
 			.findOne()
 			.where('slug', locals.filters.menu)
-			// .populate('pages')
 			.exec(function(err, menu) {
 				if (err) {
 					next(err);
@@ -151,23 +149,11 @@ exports = module.exports = function(req, res) {
 						'_id': widget.get('keystoneWidget')
 					})
 					.exec(function(err, kWidget) {
-						var view;
-						if (!err && kWidget) {
-							locals.widgetTpl = kWidget.name;
+						if (!err) {
                             locals.keystoneWidget = kWidget;
-							next(); // Temp switch
-                            // try {
-							// 	view = require('../widgets/' + kWidget.name);
-							// 	view(locals.widget, next);
-							// } catch (e) {
-							// 	console.log(e);
-							// 	next(e);
-							// }
-						} else {
-							next();
 						}
+						next(err);
 					});
-				// console.log(widget.get('name'));
 				break;
 			case 'stratum':
 				keystone.list('StratumWidget').model
