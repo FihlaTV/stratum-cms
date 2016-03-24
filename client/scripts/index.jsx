f4c21214c4075c47b42937a2755ee9a9c8c72519
Fredrik Lundberg <fredrik.lundberg@registercentrum.se>
2016-03-24 10:46:44 +0100

    Removed unused package

--- client/scripts/index.jsx
+++ client/scripts/index.jsx
@@ -4,7 +4,6 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import { Provider } from 'react-redux';
 import Application from './containers/App';
-import { BOAArtrosSkola } from './widgets';
 import WidgetWrapper from './components/WidgetWrapper'
 import loginApp from './reducers/reducers';
 
