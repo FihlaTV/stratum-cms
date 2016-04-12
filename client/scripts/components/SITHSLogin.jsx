3ad86f887eefd45b9fdb73513b6a18a6c5300123
Fredrik Lundberg <fredrik.lundberg@registercentrum.se>
2016-04-12 17:04:48 +0200

    Changed information texts to new alternatives

--- client/scripts/components/SITHSLogin.jsx
+++ client/scripts/components/SITHSLogin.jsx
@@ -9,8 +9,7 @@ const SITHSLogin = ({
 		case 'SITHS_INTRO':
 			return (
 				<Alert alertType="info" faIcon="fa-info">
-					<p><strong>Först!</strong> Försäkra dig om att ditt SITHS-kort sitter i datorn</p>
-					<p>I nästa steg kommer din webbläsare att efterfråga ett certifikat, välj där det som är förknippat med ditt namn och tryck på OK</p>
+					<p><strong>Först!</strong> Se till att ditt SITHS-kort sitter i datorn. Klicka sen på Nästa</p>
 				</Alert>
 			);
 		case 'SITHS_DO_LOGIN':
