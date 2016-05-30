b9d25812a811a6b252ae40d7c5050d451695fea2
Fredrik Lundberg <fredrik.lundberg@registercentrum.se>
2016-05-30 10:54:37 +0200

    New SubRegister model

--- models/SubRegister.js
+++ models/SubRegister.js
@@ -2,10 +2,29 @@ var keystone = require('keystone'),
 	Types = keystone.Field.Types;
 
 /**
+ * Sub Register Model
  * ==========
+ * Represents a Register in stratum for use in portals
+ */
+
+var SubRegister = new keystone.List('SubRegister', {
+	sortable: true,
+	hidden: !keystone.get('is portal') // Only visible in portal mode
 });
 
 SubRegister.add({
 	name: {
+		type: String,
+		required: true,
+		initial: true,
+	},
+	url: {
+		type: Types.Url,
+		required: true,
+		initial: true
+	}
+});
+
+SubRegister.defaultColumns = 'name';
 
 SubRegister.register();
