66c733ba5121ac073e316833b08e19695b325f9b
Fredrik Lundberg <fredrik.lundberg@registercentrum.se>
2016-06-14 17:32:14 +0200

    Done a major overhaul to frontend build process
    All registers now have their own css and javascript which is depending
    on respective .env-files.
    
    It is now possible to build all registers through gulp with `build-all`
    task.
    
    All registers now needs a separate index.jsx file which imports
    javascript as well as less files if they should overwrite any styles
    etc.

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -1,81 +1,107 @@
 var path = require('path');
 var webpack = require('webpack');
+var fs = require('fs');
 var node_modules_dir = path.resolve(__dirname, 'node_modules');
-var ExtractTextPlugin = require("extract-text-webpack-plugin");
-var glob = require('glob');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var dotenv = require('dotenv');
 
-require('dotenv').load();
-
-var jsEntry = {
-	app: path.resolve(__dirname, 'client/scripts/index.jsx'),
-	vendors: ['react', 'jquery', 'react-bootstrap', 'react-dom', 'redux']
-};
-
-function findRegisters() {
-	var registerStyles = jsEntry;
-	glob.sync('registers/*/override/styles/site.less').forEach(function (stylePath) {
-		var match = /^registers\/([^\/]+)\//.exec(stylePath);
-		if (match) {
-			registerStyles[match[1]] = path.resolve(__dirname, stylePath);
-	}
-	});
-	return registerStyles;
+function generateConfig(indexPath, brand, envVars){
+	return {
+		entry: {
+			base: indexPath,
+			vendors: ['react', 'jquery', 'react-bootstrap', 'react-dom', 'redux']
+		},
+		output: {
+			path: path.resolve(__dirname, 'public/dist/'),
+			filename: brand + '.bundle.js'
+		},
+		plugins: [
+			new webpack.DefinePlugin({
+				'process.env': Object.keys(envVars).reduce(function (o, k) {
+					//Whitelist to variables only prefixed with CLIENT_
+					if (k.indexOf('CLIENT_') === 0) {
+						o[k] = JSON.stringify(envVars[k]);
+					}
+					return o;
+				}, {})
+			}),
+			new webpack.DefinePlugin({
+				'process.env.NODE_ENV': '"production"'
+			}),
+			new webpack.ProvidePlugin({
+				$: 'jquery',
+				jQuery: 'jquery'
+			}),
+			new webpack.optimize.UglifyJsPlugin(),
+			new ExtractTextPlugin(brand + '.styles.css'),
+			new webpack.optimize.CommonsChunkPlugin('vendors', 'vendors.js')
+		],
+		module: {
+			loaders: [{
+				test: /\.jsx{0,1}$/,
+				loader: 'babel-loader',
+				exclude: [node_modules_dir],
+				query: {
+					presets: ['es2015', 'react'],
+					plugins: ['transform-object-rest-spread', 'transform-object-assign']
+				}
+			}, {
+					test: /\.less$/,
+					loader: ExtractTextPlugin.extract(['css', 'less'])
+				}, {
+					test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
+					loader: 'url?limit=10000&mimetype=application/font-woff'
+				}, {
+					test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+					loader: 'url?limit=10000&mimetype=application/octet-stream'
+				}, {
+					test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+					loader: 'file'
+				}, {
+					test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+					loader: 'url?limit=10000&mimetype=image/svg+xml'
+				}]
+		},
+		resolve: {
+			extensions: ['', '.js', '.jsx']
+		}
+	};
 }
 
+function readEnvironmentVariables(filename){
+	return dotenv.parse(fs.readFileSync(filename));
+}
 
-module.exports = {
-    entry: findRegisters(),
-    output: {
-        path: path.resolve(__dirname, 'public/dist/'),
-        filename: '[name].bundle.js'
-    },
-    plugins: [
-        new webpack.DefinePlugin({
-            'process.env': Object.keys(process.env).reduce(function (o, k) {
-                //Whitelist to variables only prefixed with CLIENT_
-                if (k.indexOf('CLIENT_') === 0) {
-                    o[k] = JSON.stringify(process.env[k]);
-                }
-                return o;
-            }, {})
-        }),
-		new webpack.DefinePlugin({
-			'process.env.NODE_ENV': '"production"'
-		}),
-		new webpack.ProvidePlugin({
-			$: "jquery",
-			jQuery: "jquery"
-		}),
-		new ExtractTextPlugin('[name].styles.css'),
-        new webpack.optimize.CommonsChunkPlugin('vendors', 'vendors.js')
-    ],
-    module: {
-        loaders: [{
-            test: /\.jsx{0,1}$/,
-            loader: 'babel-loader',
-            exclude: [node_modules_dir],
-            query: {
-                presets: ['es2015', 'react'],
-				plugins: ['transform-object-rest-spread', 'transform-object-assign']
-            }
-		}, {
-				test: /\.less$/,
-				loader: ExtractTextPlugin.extract(['css', 'less'])
-			}, {
-				test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
-				loader: 'url?limit=10000&mimetype=application/font-woff'
-			}, {
-				test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-				loader: 'url?limit=10000&mimetype=application/octet-stream'
-			}, {
-				test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
-				loader: 'file'
-			}, {
-				test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-				loader: 'url?limit=10000&mimetype=image/svg+xml'
-			}]
-    },
-    resolve: {
-        extensions: ['', '.js', '.jsx']
-    }
+function initConfig (_indexPath) {
+	var regPath, envVars = {};
+	if(_indexPath){
+		regPath = path.resolve(__dirname, _indexPath);
+	} else {
+		regPath = path.resolve(__dirname, process.argv.indexOf('--env.regPath') >= 0 ? process.argv[process.argv.indexOf('--env.regPath') + 1] : '.');
+	}
+	try {
+		fs.accessSync(path.resolve(regPath, '.env'), fs.F_OK);
+		envVars = readEnvironmentVariables(path.resolve(regPath, '.env'));
+	} catch (e) {
+		console.log('Missing .env file in ' + regPath + ' or unable to parse .env file...');
+		process.exit();
+	}
+	
+	// Should result in the same value as 'brand safe' in keystone
+	var brandSafe = (envVars.BRAND || 'app').trim().replace(/\W+/g, '-').toLowerCase();
+
+	var indexPath = path.resolve(regPath, 'index.jsx');
+
+	// Check for a local index.jsx file
+	try {
+		fs.accessSync(indexPath, fs.F_OK);
+	} catch (e) {
+		// Default to the root index.jsx file
+		indexPath = path.resolve(__dirname, 'client/scripts/index.jsx');
+	}
+
+	return generateConfig(indexPath, brandSafe, envVars);
 };
+
+module.exports = initConfig();
+module.exports.initConfig = initConfig;
