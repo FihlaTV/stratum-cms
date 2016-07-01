e1c4176fcfef45d110b0ff118a97f9d6e137d10a
Fredrik Lundberg <fredrik.lundberg@registercentrum.se>
2016-07-01 17:45:33 +0200

    Added model for messages
    Date picker seems a bit buggy when i comes to the following format

---
+++ models/Message.js
@@ -0,0 +1,36 @@
+var keystone = require('keystone'),
+	Types = keystone.Field.Types;
+
+/**
+ * Message Model
+ * ==========
+ */
+
+var Message = new keystone.List('Message', {
+	map: { name: 'title' },
+	track: { createdAt: true, updatedAt: true, updatedBy: true },
+	sortable: true
+});
+
+var dateFormat = 'YYYY-DD-MM HH:mm:ss';
+
+Message.add({
+	title: { type: String, required: true },
+	message: { type: Types.Textarea, height: 150 },
+	startTime: {
+		type: Types.Datetime,
+		default: Date.now,
+		note: 'Determines when the message should start showing',
+		format: dateFormat
+	},
+	endTime: {
+		type: Types.Datetime,
+		note: 'Determines after which time the message should no longer be displayed. Both `startTime` and `endTime` is required for the message to show at all',
+		format: dateFormat
+	},
+	status: { type: Types.Select, options: 'info, warning, danger, success', default: 'info' },
+	dismissible: { type: Boolean, default: true, note: 'Uncheck this if the user shouldn\'t be able to close the message' }
+});
+
+Message.defaultColumns = 'title, startTime|20%, endTime|20%, status|20%';
+Message.register();
