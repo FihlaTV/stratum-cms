9fef93293135c1a66ef19ace194ae552aaf52ff4
Fredrik Lundberg <fredrik.lundberg@registercentrum.se>
2016-09-21 08:47:03 +0200

    Added support for assign siths in login proxy

--- routes/api/stratum-login.js
+++ routes/api/stratum-login.js
@@ -3,6 +3,34 @@ var keystone = require('keystone'),
 	request = require('request'),
 	zlib = require('zlib');
 
+/**
+ * Returns a function which performs a GET or POST 
+ * request and gunzips the response if necessary.
+ * 
+ * @param conf config object for request
+ * @param post boolean determines if the request should be a post call or get call.
+ */
+function doRequest(conf, res, req, post) {
+	var reqFunc = post ? request.post : request;
+	return reqFunc(conf, function (err, stratumRes, body) {
+		if (!err && body) {
+			if (stratumRes.headers['content-encoding'] === 'gzip') {
+				zlib.gunzip(body, function (err, dezipped) {
+					parseResponse(req, res, dezipped.toString('utf-8'));					
+				});
+			} else {
+				parseResponse(req, res, body);
+			}
+		} else {
+			return res.status(500).apiResponse({
+				success: false,
+				code: 'UNKNOWN_STRATUM_ERROR',
+				message: 'Server error'
+			});
+		}
+	});
+}
+
 function parseResponse(req, res, body) {
 	var jsonBody;
 	try {
@@ -37,8 +65,10 @@ exports = module.exports = function (req, res) {
 		protocol = referer ? referer.split('/')[0] :
 			req.secure ? 'https:' : 'http:',
 		stratumServer = protocol + '//' + keystone.get('stratum server'),
-
-		apiUrl;
+		conf = {
+			rejectUnauthorized: false,
+			encoding: null
+		};
 
 	if (!stratumServer) {
 		return res.apiResponse({
@@ -48,27 +78,15 @@ exports = module.exports = function (req, res) {
 	}
 	// req.url should be either /api/authentication/context or /api/authentication/login
 	// might be better to hard code these
-	apiUrl = url.resolve(stratumServer, req.url);
-
-	req.pipe(request({
-		rejectUnauthorized: false,
-		url: apiUrl,
-		encoding: null
-	}, function (err, stratumRes, body) {
-		if (!err && body) {
-			if (stratumRes.headers['content-encoding'] === 'gzip') {
-				zlib.gunzip(body, function (err, dezipped) {
-					parseResponse(req, res,  dezipped.toString('utf-8'));					
-				});
-			} else {
-				parseResponse(req, res, body);
-			}
-		} else {
-			return res.status(500).apiResponse({
-				success: false,
-				code: 'UNKNOWN_STRATUM_ERROR',
-				message: 'Server error'
-			});
-		}
-	}));
+	conf.uri = url.resolve(stratumServer, req.url);
+	if(req.method === 'POST'){
+		conf.headers = {
+			'Cookie': req.headers['cookie'],
+			'User-Agent': req.headers['user-agent'],
+		};
+		conf.form = req.body;
+		doRequest(conf, res, req, true);
+		return;
+	}
+	req.pipe(doRequest(conf, res, req));
 };
