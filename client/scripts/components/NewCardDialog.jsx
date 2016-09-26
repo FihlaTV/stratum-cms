1394d5af3d9356a98614cd0d444510255f1b42b8
Fredrik Lundberg <fredrik.lundberg@registercentrum.se>
2016-09-26 14:53:45 +0200

    Fixed linting problems

--- client/scripts/components/NewCardDialog.jsx
+++ client/scripts/components/NewCardDialog.jsx
@@ -1,71 +1,71 @@
-import React, { Component, PropTypes } from 'react';
+import React, { PropTypes } from 'react';
 import { FormGroup, FormControl, ControlLabel, HelpBlock } from 'react-bootstrap';
 import ReactDOM from 'react-dom';
 
-function FieldGroup({ id, label, help, refInput, style, inputStyle, ...props }) {
-  return (
-    <FormGroup controlId={id} style={style}>
-      <ControlLabel>{label}</ControlLabel>
-      <FormControl ref={refInput} style={inputStyle} {...props} />
-      {help && <HelpBlock>{help}</HelpBlock>}
-    </FormGroup>
+function FieldGroup ({ id, label, help, refInput, style, inputStyle, ...props }) {
+	return (
+		<FormGroup controlId={id} style={style}>
+			<ControlLabel>{label}</ControlLabel>
+			<FormControl ref={refInput} style={inputStyle} {...props} />
+			{help && <HelpBlock>{help}</HelpBlock>}
+		</FormGroup>
   );
 }
 
 const NewCardDialog = ({
-        onChange,
+		onChange,
 		onSubmit,
-		valid
+		valid,
 	}) => {
-	
+
 	let username, password;
-	
+
 	return (
-		<form 
+		<form
 			onSubmit={(e) => {
 				e.preventDefault();
 				var usernameVal = ReactDOM.findDOMNode(username).value;
 				var passwordVal = ReactDOM.findDOMNode(password).value;
 				return onSubmit(usernameVal, passwordVal);
 			}}>
-			<FieldGroup 
+			<FieldGroup
 				label="Användarnamn:"
-				autoComplete="off" 
+				autoComplete="off"
 				id="assignUsername"
 				onChange={
 					(e) => {
 						const passwordVal = ReactDOM.findDOMNode(password).value;
-						return onChange({username: e.target.value, password: passwordVal});
-					}} 
-				refInput={(node) => username = node}
+						return onChange({ username: e.target.value, password: passwordVal });
+					}}
+				refInput={(node) => { username = node; }}
 				autoFocus
 			/>
 			<FieldGroup
 				label="Engångslösenord:"
-				type="password" 
-				autoComplete="off" 
-				id="assignPassword" 
+				type="password"
+				autoComplete="off"
+				id="assignPassword"
 				onChange={
 					(e) => {
 						const usernameVal = ReactDOM.findDOMNode(username).value;
-						return onChange({username: usernameVal, password: e.target.value});
-					}} 
-				refInput={(node) => password = node}
+						return onChange({ username: usernameVal, password: e.target.value });
+					}}
+				refInput={(node) => { password = node; }}
 			/>
 			<FieldGroup
 				type="submit"
 				style={{
-					display: 'none'
+					display: 'none',
 				}}
 			/>
 		</form>
 	);
-	
+
 };
 
 NewCardDialog.propTypes = {
+	onChange: PropTypes.func,
 	onSubmit: PropTypes.func.isRequired,
-	onChange: PropTypes.func
 };
 
  export default NewCardDialog;
