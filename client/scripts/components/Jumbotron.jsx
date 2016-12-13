2cee9456e4d7a67a6ec1a12adf826f2ac5060db7
Fredrik Lundberg <fredrik.lundberg@registercentrum.se>
2016-12-13 16:40:05 +0100

    KeystoneWidgets are now functional on StartPage

--- client/scripts/components/Jumbotron.jsx
+++ client/scripts/components/Jumbotron.jsx
@@ -3,13 +3,13 @@ import { Row } from 'react-bootstrap';
 import StartPageWidget from './StartPageWidget';
 
 const Jumbotron = ({
-	header = 'Jumbotron',
-	description = '<p><strong>Lead HTML text</strong></p>',
+	header,
+	description,
 	widgets = [],
 }) => (
 	<div className="jumbotron">
 		<h1>{header}</h1>
-		<div className="lead" dangerouslySetInnerHTML={{ __html: description.html }}/>
+		{description && <div className="lead" dangerouslySetInnerHTML={{ __html: description.html }}/>}
 		<Row>
 			{widgets.map(({ slug, digit, description, keystoneWidget }) =>
 				<StartPageWidget keystoneWidget={keystoneWidget} key={slug} digit={digit} description={description}/>
--- client/scripts/components/StartPageWidget.jsx
+++ client/scripts/components/StartPageWidget.jsx
@@ -1,16 +1,34 @@
 import React from 'react';
 import { Col } from 'react-bootstrap';
+import WidgetWrapper from './WidgetWrapper';
 
-const StartPageWidget = ({ digit, description, link, linkText, keystoneWidget }) => (
+const WidgetContainer = ({ link, linkText, children }) =>
+(
 	<Col className="startpage-widget" md={3} sm={4}>
-		<span className="startpage-widget-digit">
-			{digit}
-		</span>
-		<span className="startpage-widget-text">
-			{description}
-		</span>
-		<a href={link}>{linkText}</a>
+		{children}
+		{link && linkText && <a href={link}>{linkText}</a>}
 	</Col>
 );
+
+const StartPageWidget = ({ digit, description, link, linkText, keystoneWidget }) =>
+{
+	if (keystoneWidget) {
+		return (
+			<WidgetContainer link={link} linkText={linkText}>
+				<WidgetWrapper id={keystoneWidget} description={description} />
+			</WidgetContainer>
+		);
+	}
+	return (
+		<WidgetContainer link={link} linkText={linkText}>
+			<span className="startpage-widget-digit">
+				{digit}
+			</span>
+			<span className="startpage-widget-text">
+				{description}
+			</span>
+		</WidgetContainer>
+	);
+};
 
 export default StartPageWidget;
