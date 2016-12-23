39fb2210be668dd3b97980ee1e02ea21ec826748
Fredrik Lundberg <fredrik.lundberg@registercentrum.se>
2016-12-23 11:36:41 +0100

    Stratum proxy now runs before routing
    All requests are now also handled equally

--- routes/proxies/stratum.js
+++ routes/proxies/stratum.js
@@ -1,12 +1,18 @@
 var request = require('request');
 var	keystone = require('keystone');
 
-exports = module.exports = function (req, res) {
-	var referer = req.header('referer');
-	var isDemo = keystone.get('is demo');
+exports = module.exports = function (req, res, next) {
+	var referer;
+	var isDemo;
 	var protocol;
 	var	stratumUrl;
 	var uri;
+
+	if (!(/^\/stratum\/[^\/]/).test(req.url)) {
+		return next();
+	}
+	referer = req.header('referer');
+	isDemo = keystone.get('is demo');
 
 	if (isDemo) {
 		protocol = 'http:';
@@ -18,34 +24,8 @@ exports = module.exports = function (req, res) {
 	// Might be a good idea to have a more fail safe approach to this string concatenation.
 	stratumUrl = protocol + '//' + keystone.get('stratum server') + '/';
 
-	if (!(/^\/stratum\/[^\/]/).test(req.url)) {
-		res.notFound();
-		return;
-	}
 	uri = req.url.replace(/^\/stratum\//, stratumUrl);
 
-	// Special handling for REST verbs and Direct calls
-	if (['POST', 'PUT'].indexOf(req.method) !== -1) {
-		// body-parser prevents the simple proxy to work.
-		// https://github.com/request/request/issues/1664
-		// http://stackoverflow.com/questions/26121830/proxy-json-requests-with-node-express.
-		// This could be solved (and be more  efficient) if done earlier in the call chain, to avoid body-parser from
-		// intercepting and parsing the request first.
-		request({
-			uri: uri,
-			rejectUnauthorized: false,
-			method: req.method,
-			encoding: null,
-			form: req.headers['content-type'].indexOf('application/json') === 0 ? JSON.stringify(req.body) : req.body,
-			headers: {
-				'Cookie': req.headers.cookie,
-				'User-Agent': req.headers['user-agent'],
-				'Content-Length': req.headers['content-length'],
-				'Content-Type': req.headers['content-type'],
-			},
-		}).pipe(res);
-		return;
-	}
 	req.pipe(
 		request(uri)
 	).pipe(res);
