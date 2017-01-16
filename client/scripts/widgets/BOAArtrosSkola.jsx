ada434bd792c021361b83f3db87107f24af40992
Fredrik Lundberg <fredrik.lundberg@registercentrum.se>
2017-01-16 17:45:49 +0100

    Updated BOA ArtrosSkola widget to antrop layout

--- client/scripts/widgets/BOAArtrosSkola.jsx
+++ client/scripts/widgets/BOAArtrosSkola.jsx
@@ -2,14 +2,32 @@ import React from 'react';
 import StratumSimpleData from './StratumSimpleData';
 
 const BOAArtrosSkola = ({
-	description,
 }) => {
 	return (
-		<StratumSimpleData
-			url="/stratum/api/aggregate/BOA/UnitPraxis/Total/count(UP_UnitName)?APIKey=bK3H9bwaG4o%3D"
-		>
-			<p>{description}</p>
-		</StratumSimpleData>
+		<div>
+			<div className="side-widget">
+				<StratumSimpleData
+					unstyled
+					url="/stratum/api/aggregate/BOA/UnitPraxis/Total/count(UP_UnitName)?APIKey=bK3H9bwaG4o%3D"
+					className="side-widget-digit"
+				>
+					<span className="side-widget-description">
+						av de patienter som går artrosskola mår bättre efter avslutad utbildning
+					</span>
+				</StratumSimpleData>
+			</div>
+			<div className="side-widget">
+				<StratumSimpleData
+					unstyled
+					url="/stratum/api/aggregate/BOA/UnitPraxis/Total/count(UP_UnitName)?APIKey=bK3H9bwaG4o%3D"
+					className="side-widget-digit"
+				>
+					<span className="side-widget-description">
+						mottagningar över hela landet registrerar i BOA registret idag
+					</span>
+				</StratumSimpleData>
+			</div>
+		</div>
 	);
 };
 
