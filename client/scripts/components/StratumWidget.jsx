b13af900b6e84bf4bb8cf8b32bd6b21cc5762c7b
Fredrik Lundberg <fredrik.lundberg@registercentrum.se>
2017-02-10 16:54:05 +0100

    Updated StratumWidget to work with query
    Also added possibility to add widgets without specifying container id

--- client/scripts/components/StratumWidget.jsx
+++ client/scripts/components/StratumWidget.jsx
@@ -1,8 +1,12 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import StratumLoader, { startRegistrations, startWidget } from '../utils/stratumLoader';
 import Spinner from './Spinner';
 
 const REGISTRATIONS_WIDGET_NAME = 'registrations';
+
+function getNonce () {
+	return ((new Date()).getTime() + parseInt(Math.random() * 100)).toString(36);
+}
 
 class StratumWidget extends Component {
 	constructor (props) {
@@ -11,24 +15,39 @@ class StratumWidget extends Component {
 			loading: true,
 		};
 	}
+	componentWillMount () {
+		const { target: _target } = this.props;
+		const target = _target || `stratum-widget-${getNonce()}`;
+
+		this.setState({ target });
+	}
 	componentDidMount () {
-		const { containerId, widget } = this.props;
+		const { target } = this.state;
+		const { widget, query } = this.props;
+		const queryString = this.formatQuery(query);
+
 		if (typeof window.Stratum !== 'undefined' && widget) {
 			if (widget === REGISTRATIONS_WIDGET_NAME && window.Stratum.ApplicationForRegistrations) {
-				startRegistrations(containerId, (success) => this.finishedLoading(success));
+				startRegistrations(target, (success) => this.finishedLoading(success));
 			} else if (widget !== REGISTRATIONS_WIDGET_NAME) {
-				startWidget(containerId, widget);
+				startWidget(target, widget, queryString, () => this.finishedLoading());
 			}
 		} else {
-			StratumLoader(containerId, widget, null, (success) => this.finishedLoading(success));
+			StratumLoader(target, widget, queryString, (success) => this.finishedLoading(success));
 		}
 	}
 	componentWillUnmount () {
 		const { purgeOrphans } = window;
-		const { containerId } = this.props;
+		const { target } = this.state;
+
 		if (typeof purgeOrphans !== 'undefined') {
-			purgeOrphans(document.getElementById(containerId));
+			purgeOrphans(document.getElementById(target));
 		}
+	}
+	formatQuery (query) {
+		const parameters = Object.keys(query).reduce((prev, key, i) => `${prev}${i > 0 ? '&' : ''}${key}=${query[key]}`, '');
+
+		return parameters ? `?${parameters}` : '';
 	}
 	finishedLoading (success) {
 		this.setState({
@@ -36,10 +55,10 @@ class StratumWidget extends Component {
 		});
 	}
 	render () {
-		const { containerId } = this.props;
-		const { loading } = this.state;
+		const { loading, target } = this.state;
+
 		return (
-			<div id={containerId}>
+			<div id={target}>
 				{loading && <Spinner />}
 			</div>
 		);
@@ -48,8 +67,14 @@ class StratumWidget extends Component {
 
 StratumWidget.defaultProps = {
 	registrationId: `registration-script-${(new Date()).getTime()}`,
-	containerId: 'sw-registrations',
 	widget: REGISTRATIONS_WIDGET_NAME,
+	query: {},
+};
+
+StratumWidget.PropTypes = {
+	target: PropTypes.string,
+	widget: PropTypes.string,
+	query: PropTypes.object,
 };
 
 export default StratumWidget;
