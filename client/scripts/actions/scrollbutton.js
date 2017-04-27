f518ff1c195c4aa3b708485585db6ccba84a6cd6
namnkunnig <magnus.kinnander@gmail.com>
2017-04-27 16:02:20 +0200

    Updated scrollbutton

--- client/scripts/actions/scrollbutton.js
+++
@@ -1,8 +0,0 @@
-export const SHOW_SCROLL_BUTTON = 'SHOW_SCROLL_BUTTON';
-
-export function showScrollButton (show) {
-	return {
-		type: SHOW_SCROLL_BUTTON,
-		show: show,
-	};
-}
--- client/scripts/reducers/reducers.js
+++ client/scripts/reducers/reducers.js
@@ -7,7 +7,6 @@ import menu from './menu';
 import page from './page';
 import error from './error';
 import faq from './faq';
-import scrollbutton from './scrollbutton';
 import registerInformation from './registerInformation';
 import startPage from './startPage';
 import breadcrumbs from './breadcrumbs';
@@ -29,5 +28,4 @@ export default combineReducers({
 	registerInformation,
 	breadcrumbs,
 	startPage,
-	scrollbutton,
 });
