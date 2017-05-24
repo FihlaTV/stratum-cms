4f444c40a6e7924546b5353764d951671e44a671
Fredrik Lundberg <fredrik.lundberg@registercentrum.se>
2017-05-24 16:31:03 +0200

    Fix linting error

--- client/scripts/components/Menu.jsx
+++ client/scripts/components/Menu.jsx
@@ -1,4 +1,4 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
 import { Navbar, Nav, NavItem, NavDropdown } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 import { IndexLink } from 'react-router';
