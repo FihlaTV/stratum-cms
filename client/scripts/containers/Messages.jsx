14662b91c8398ca2cdaac9d1fd282d3aecb6faad
Fredrik Lundberg <fredrik.lundberg@registercentrum.se>
2017-06-21 16:43:11 +0200

    Upped version of react Changed to new transition group

--- client/scripts/containers/Messages.jsx
+++ client/scripts/containers/Messages.jsx
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 import { showMessage, initMessages, acceptCookie } from '../actions/messages';
 import Message from '../components/Message';
 import CookieMessage from '../components/CookieMessage';
-import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+import { CSSTransitionGroup } from 'react-transition-group';
 
 class Messages extends Component {
 	componentDidMount () {
@@ -20,7 +20,7 @@ class Messages extends Component {
 
 		return (
 			<div {...rest}>
-				<ReactCSSTransitionGroup transitionName="alert-transition" transitionAppear transitionAppearTimeout={300} transitionEnterTimeout={300} transitionLeaveTimeout={300}>
+				<CSSTransitionGroup transitionName="alert-transition" transitionAppear transitionAppearTimeout={300} transitionEnterTimeout={300} transitionLeaveTimeout={300}>
 					{showCookieMessage && <CookieMessage key="cookie-message" onDismiss={
 							() => dispatch(acceptCookie())
 						}
@@ -35,7 +35,7 @@ class Messages extends Component {
 									}/>
 							)
 					}
-				</ReactCSSTransitionGroup>
+				</CSSTransitionGroup>
 			</div>
 		);
 	}
