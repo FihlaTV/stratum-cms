bbf746bd98488c0bb0ea5075ef859aef90138c8d
Magnus Kinnander <namnkunnig@users.noreply.github.com>
2017-06-22 15:40:42 +0200

    Refactored scrollbutton

--- client/scripts/components/ScrollButton.jsx
+++ client/scripts/components/ScrollButton.jsx
@@ -2,6 +2,6 @@ import React from 'react';
 
 const ScrollButton = () => {
 	var pageIsCropped = document.body.scrollHeight > window.innerHeight ? '' : 'hidden';
-	return (<a onClick={function scrollToTop() { scroll(0, 0); }} className={`scrollbutton ${pageIsCropped}`}>UPP</a>);
+	return (<a onClick={function scrollToTop () { scroll(0, 0); }} className={`scrollbutton ${pageIsCropped}`}>UPP</a>);
 };
 export default ScrollButton;
