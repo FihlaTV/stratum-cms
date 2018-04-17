b0d771ff8c70308f330849fe0e54fb59a0de9cdf
Fredrik Lundberg <fredrik@ablu.se>
2018-04-17 18:40:15 +0200

    Added groups to contacts api

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,9 +1,21 @@
 const keystone = require('keystone');
 const formatCloudinaryImage = require('../../utils/format-cloudinary-image');
 
-const formatContact = ({ email, phone, title, image, name, note }) => {
+const formatContact = ({
+	email,
+	phone,
+	title,
+	image,
+	name,
+	note,
+	groups = [],
+}) => {
 	let retObj = {
 		email,
+		groups: groups.map(({ group, slug }) => ({
+			group,
+			id: slug,
+		})),
 		phone,
 		title,
 		name,
@@ -22,7 +34,8 @@ exports = module.exports = function(req, res) {
 	keystone
 		.list('Contact')
 		.model.where('showOnContactPage', true)
-		.select('email phone title note image name')
+		.select('email phone title note image name groups')
+		.populate('groups', 'group slug')
 		.exec((err, contacts) => {
 			if (err) {
 				return res.apiResponse({
